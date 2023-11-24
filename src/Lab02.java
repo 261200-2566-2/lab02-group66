7d63a0aa8b8bcbab49bb75078b781bf559ac337e
ColtenMore <aungpao321@gmail.com>
2023-11-24 13:54:40 +0700

    commit2

--- src/Lab02.java
+++ src/Lab02.java
@@ -1,14 +1,12 @@
 public class Lab02 {
     public static void main(String[] args) {
-        AirPurifier a = new AirPurifier("abcde","12345");
+        AirPurifier a = new AirPurifier("abcde","12345a");
         AirPurifier.totalAirPurifiers();
         a.turnOn();
         AirPurifier.giveInfo(a);
-
-        AirPurifier b = new AirPurifier("fghij","67890");
+        AirPurifier b = new AirPurifier("fghij","67890b");
         AirPurifier.totalAirPurifiers();
         AirPurifier.giveInfo(b);
-
         a.turnOff();
         b.turnOn();
         AirPurifier.giveInfo(a);
